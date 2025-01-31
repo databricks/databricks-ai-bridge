1e49ca8664d920b3eec53494c963e47295cc74f9
Ann Zhang <ann.zhang@databricks.com>
2025-01-31 12:12:58 -0800

    update
    Signed-off-by: Ann Zhang <ann.zhang@databricks.com>

---
+++ src/databricks_ai_bridge/__init__.py
@@ -0,0 +1,7 @@
+from unitycatalog.ai.core.databricks import DatabricksFunctionClient
+from unitycatalog.ai.core.base import set_uc_function_client
+
+__all__ = [
+    "DatabricksFunctionClient",
+    "set_uc_function_client",
+]
