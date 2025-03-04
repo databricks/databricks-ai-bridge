ed3be5b2d9f30d6c49e0283fe61a5d6ca3b810cd
Ann Zhang <ann.zhang@databricks.com>
2025-03-04 09:39:32 -0800

    update
    Signed-off-by: Ann Zhang <ann.zhang@databricks.com>

--- integrations/openai/src/databricks_openai/__init__.py
+++ integrations/openai/src/databricks_openai/__init__.py
@@ -1,6 +1,12 @@
 from databricks_openai.vector_search_retriever_tool import VectorSearchRetrieverTool
+from unitycatalog.ai.core.base import set_uc_function_client
+from unitycatalog.ai.core.databricks import DatabricksFunctionClient
+from unitycatalog.ai.openai.toolkit import UCFunctionToolkit
 
 # Expose all integrations to users under databricks-openai
 __all__ = [
     "VectorSearchRetrieverTool",
+    "UCFunctionToolkit",
+    "DatabricksFunctionClient",
+    "set_uc_function_client",
 ]
