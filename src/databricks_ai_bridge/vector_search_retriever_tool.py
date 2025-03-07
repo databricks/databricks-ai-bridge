87369d6c530bdb7e1648dd0713001ef6be60787a
Ann Zhang <ann.zhang@databricks.com>
2025-03-07 10:50:58 -0800

    64 chars
    Signed-off-by: Ann Zhang <ann.zhang@databricks.com>

--- src/databricks_ai_bridge/vector_search_retriever_tool.py
+++ src/databricks_ai_bridge/vector_search_retriever_tool.py
@@ -69,9 +69,9 @@ class VectorSearchRetrieverToolMixin(BaseModel):
     @validator("tool_name")
     def validate_tool_name(cls, tool_name):
         if tool_name is not None:
-            pattern = re.compile(r"^[a-zA-Z0-9_-]+$")
+            pattern = re.compile(r"^[a-zA-Z0-9_-]{1,64}$")
             if not pattern.fullmatch(tool_name):
-                raise ValueError("tool_name must match the pattern '^[a-zA-Z0-9_-]$'")
+                raise ValueError("tool_name must match the pattern '^[a-zA-Z0-9_-]{1,64}$'")
         return tool_name
 
     def _get_default_tool_description(self, index_details: IndexDetails) -> str:
