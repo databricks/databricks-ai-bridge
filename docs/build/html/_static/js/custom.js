7275913e19437bd372774ceb3244ba2eb600197b
Ann Zhang <ann.zhang@databricks.com>
2025-05-13 14:03:30 -0700

    update dev versions
    Signed-off-by: Ann Zhang <ann.zhang@databricks.com>

--- docs/build/html/_static/js/custom.js
+++
@@ -1,5 +0,0 @@
-/* This adds target='_blank' to all external links,
-   allowing for them to open in a new browser window/tab. */
-$(document).ready(function () {
-    $('a.external').attr('target', '_blank');
-});
