cd6ca7b60cfc7e0d437b475d76a3d03b12ec4ff7
Bradley Jamrozik <bradleyjamrozik@ozinga.com>
2025-07-23 14:38:01 -0500

    Allow genie results to be returned as a json string instead of only a markdown string.

--- src/databricks_ai_bridge/genie.py
+++ src/databricks_ai_bridge/genie.py
@@ -4,6 +4,7 @@ import time
 from dataclasses import dataclass
 from datetime import datetime
 from typing import Optional, Union
+import json
 
 import mlflow
 import pandas as pd
@@ -19,6 +20,9 @@ def _count_tokens(text):
     encoding = tiktoken.encoding_for_model("gpt-4o")
     return len(encoding.encode(text))
 
+def _to_json_string(data: pd.DataFrame) -> str:
+    json_data = data.to_json(orient="records")
+    return json.dumps(json_data)
 
 @dataclass
 class GenieResponse:
@@ -91,6 +95,69 @@ def _parse_query_result(resp) -> Union[str, pd.DataFrame]:
 
     return truncated_result.strip()
 
+@mlflow.trace(span_type="PARSER")
+def _parse_query_result_json(resp) -> Union[str, pd.DataFrame]:
+    output = resp["result"]
+    if not output:
+        return "EMPTY"
+
+    columns = resp["manifest"]["schema"]["columns"]
+    header = [str(col["name"]) for col in columns]
+    rows = []
+
+    for item in output["data_array"]:
+        row = []
+        for column, value in zip(columns, item):
+            type_name = column["type_name"]
+            if value is None:
+                row.append(None)
+                continue
+
+            if type_name in ["INT", "LONG", "SHORT", "BYTE"]:
+                row.append(int(value))
+            elif type_name in ["FLOAT", "DOUBLE", "DECIMAL"]:
+                row.append(float(value))
+            elif type_name == "BOOLEAN":
+                row.append(value.lower() == "true")
+            elif type_name == "DATE" or type_name == "TIMESTAMP":
+                row.append(datetime.strptime(value[:10], "%Y-%m-%d").date())
+            elif type_name == "BINARY":
+                row.append(bytes(value, "utf-8"))
+            else:
+                row.append(value)
+
+        rows.append(row)
+
+    dataframe = pd.DataFrame(rows, columns=header)
+    query_result = _to_json_string(dataframe)
+    tokens_used = _count_tokens(query_result)
+
+    # If the full result fits, return it
+    if tokens_used <= MAX_TOKENS_OF_DATA:
+        return query_result.strip()
+
+    def is_too_big(n):
+        return _count_tokens(_to_json_string(dataframe.iloc[:n])) > MAX_TOKENS_OF_DATA
+
+    # Use bisect_left to find the cutoff point of rows within the max token data limit in a O(log n) complexity
+    # Passing True, as this is the target value we are looking for when _is_too_big returns
+    cutoff = bisect.bisect_left(range(len(dataframe) + 1), True, key=is_too_big)
+
+    # Slice to the found limit
+    truncated_df = dataframe.iloc[:cutoff]
+
+    # Edge case: Cannot return any rows because of tokens so return an empty string
+    if len(truncated_df) == 0:
+        return ""
+
+    truncated_result = _to_json_string(truncated_df)
+
+    # Double-check edge case if we overshot by one
+    if _count_tokens(truncated_result) > MAX_TOKENS_OF_DATA:
+        truncated_result = _to_json_string(truncated_df.iloc[:-1])
+
+    return truncated_result.strip()
+
 
 class Genie:
     def __init__(self, space_id, client: Optional["WorkspaceClient"] = None):
@@ -124,7 +191,7 @@ class Genie:
         return resp
 
     @mlflow.trace()
-    def poll_for_result(self, conversation_id, message_id):
+    def poll_for_result(self, conversation_id, message_id, return_data_as_json: bool = False):
         @mlflow.trace()
         def poll_query_results(attachment_id, query_str, description, conversation_id=conversation_id):
             iteration_count = 0
@@ -138,7 +205,8 @@ class Genie:
                 state = resp["status"]["state"]
                 returned_conversation_id = resp.get("conversation_id", None)
                 if state == "SUCCEEDED":
-                    result = _parse_query_result(resp)
+                    do_parse = _parse_query_result_json if return_data_as_json is True else _parse_query_result
+                    result = do_parse(resp)
                     return GenieResponse(result, query_str, description, returned_conversation_id)
                 elif state in ["RUNNING", "PENDING"]:
                     logging.debug("Waiting for query result...")
@@ -196,7 +264,7 @@ class Genie:
         return poll_result()
 
     @mlflow.trace()
-    def ask_question(self, question, conversation_id: Optional[str] = None):
+    def ask_question(self, question, conversation_id: Optional[str] = None, result_as_json: bool = False):
         # check if a conversation_id is supplied
         # if yes, continue an existing genie conversation
         # otherwise start a new conversation
@@ -204,7 +272,7 @@ class Genie:
             resp = self.start_conversation(question)
         else:
             resp = self.create_message(conversation_id, question)
-        genie_response =  self.poll_for_result(resp["conversation_id"], resp["message_id"])
+        genie_response =  self.poll_for_result(resp["conversation_id"], resp["message_id"], result_as_json)
         if not genie_response.conversation_id:
             genie_response.conversation_id = resp["conversation_id"]
         return genie_response
