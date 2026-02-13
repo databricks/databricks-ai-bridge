6613060941f1582571f8444f38bebe99c46fee39
coltonpeltier-db <colton.peltier@databricks.com>
2026-02-13 11:58:32 +0000

    move sql generation into GenericCheckpoint class

--- src/databricks_ai_bridge/lakebase_checkpointer.py
+++ src/databricks_ai_bridge/lakebase_checkpointer.py
@@ -3,34 +3,37 @@ from datetime import datetime
 from .lakebase import LakebaseClient
 from typing import Tuple
 from psycopg.types.json import Json
+from abc import ABC, abstractmethod
+from typing import Any, Union, Tuple
+from uuid import uuid4
 
-class Checkpoint(BaseModel):
-    id : str
-    state : dict
-    creation_timestamp : datetime
-    update_timestamp : datetime
-    def get_insert_params(self) -> Tuple:
-        return (
-            self.id,
-            Json(self.state),
-            self.creation_timestamp,
-            self.update_timestamp
-        )
-    def get_update_params(self) -> Tuple:
-        return (
-            Json(self.state),
-            self.update_timestamp
-        )
-
-class LakebaseCheckpointer:
-    def __init__(self, lakebase_client : LakebaseClient, sessions_table_name : str):
-        self.client = lakebase_client
-        self.table_name = sessions_table_name
-    
-    def init_schema(self) -> None:
+class GenericCheckpoint(BaseModel):
+    # Define the fields for your checkpoint
+    id : str = str(uuid4())
+    state : dict = {}
+    creation_timestamp : datetime = datetime.now()
+    update_timestamp : datetime = datetime.now()
+    # Implement abstract methods
+    def generate_insert_sql(self, table_name : str) -> Tuple[str, tuple]:
         sql = f"""
-        CREATE TABLE IF NOT EXISTS {self.table_name} (
-            lb_id bigserial PRIMARY_KEY,
+            INSERT INTO {table_name}
+            (id, state, creation_timestamp, update_timestamp)
+            VALUES (%s, %s, %s, %s)
+        """
+        return sql, (self.id, Json(self.state), self.creation_timestamp, self.update_timestamp)
+    
+    def generate_update_sql(self, table_name : str) -> Tuple[str, tuple]:
+        sql = f"""
+            UPDATE {table_name}
+            SET state = %s, update_timestamp = %s
+            WHERE id = %s AND creation_timestamp = %s
+        """
+        return sql, (Json(self.state), self.update_timestamp, self.id, self.creation_timestamp)
+    
+    def generate_init_sql(self, table_name : str) -> Tuple[str, tuple]:
+        sql = f"""
+        CREATE TABLE IF NOT EXISTS {table_name} (
+            lb_id bigserial PRIMARY KEY,
             id text NOT NULL,
             state jsonb NOT NULL default '{{}}',
             creation_timestamp timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -38,20 +41,40 @@ class LakebaseCheckpointer:
             UNIQUE(id,creation_timestamp)
         )
         """
-        response = self.client.execute(sql=sql)
+        return sql, None
     
-    def get_most_recent_checkpoint(self, id : str) -> Checkpoint | None:
-        # Get the most recently updated checkpoint for this id
+    def generate_retrieve_checkpoint_sql(self, table_name : str) -> Tuple[str, tuple]:
         sql = f"""
             SELECT id, state, creation_timestamp, update_timestamp
-            FROM {self.table_name}
+            FROM {table_name}
             WHERE id = %s
             ORDER BY update_timestamp DESC
             LIMIT 1
         """
-        response = self.client.execute(sql=sql, params = (id,))
+        return sql, (self.id,)
+
+class LakebaseCheckpointer:
+    def __init__(self, 
+                 lakebase_client : LakebaseClient, 
+                 sessions_table_name : str, 
+                 checkpoint_class : GenericCheckpoint = GenericCheckpoint):
+        self.checkpoint_class = checkpoint_class
+        self.client = lakebase_client
+        self.table_name = sessions_table_name
+        self.init_schema()
+    
+    def init_schema(self) -> None:
+        _checkpoint = self.checkpoint_class()
+        sql, params = _checkpoint.generate_init_sql(table_name = self.table_name)
+        response = self.client.execute(sql=sql, params = params)
+    
+    def get_most_recent_checkpoint(self, id : str) -> GenericCheckpoint | None:
+        _checkpoint = self.checkpoint_class(id = id)
+        # Get the most recently updated checkpoint for this id
+        sql, params = _checkpoint.generate_retrieve_checkpoint_sql(table_name = self.table_name)
+        response = self.client.execute(sql=sql, params = params)[0]
         if response:
-            response = Checkpoint(**response)
+            response = self.checkpoint_class(**response)
         return response
 
     def checkpoint_exists(self, id : str) -> bool:
@@ -70,23 +93,17 @@ class LakebaseCheckpointer:
         # Set the new state value locally
         _checkpoint.state = state
         _checkpoint.update_timestamp = datetime.now()
-        sql = f"""
-            UPDATE {self.table_name}
-            SET state = %s, update_timestamp = %s
-            WHERE id = %s AND creation_timestamp = %s
-        """
-        self.client.execute(sql=sql, params=_checkpoint.get_update_params + (_checkpoint.id, _checkpoint.creation_timestamp))
+        sql, params = _checkpoint.generate_update_sql(table_name = self.table_name)
+        self.client.execute(sql = sql, params = params)
         return
 
 
     def insert_checkpoint(self, id : str, state : dict) -> None:
-        _checkpoint = Checkpoint(id = id, state = state, creation_timestamp = datetime.now(), update_timestamp = datetime.now())
-        sql = f"""
-            INSERT INTO {self.table_name}
-            (id, state, creation_timestamp, update_timestamp)
-            VALUES (%s, %s, %s, %s)
-        """
-        response = self.client.execute(sql=sql, params=_checkpoint.get_insert_params())
+        _checkpoint = self.checkpoint_class(
+            id = id, state = state, creation_timestamp = datetime.now(), update_timestamp = datetime.now()
+        )
+        sql, params = _checkpoint.generate_insert_sql(table_name = self.table_name)
+        response = self.client.execute(sql=sql, params=params)
         return
 
 
